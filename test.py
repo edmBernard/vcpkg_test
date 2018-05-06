85d33308da2a05bd966970fee39666e0a96bb4c8
Erwan BERNARD <bernard.erwan@gmail.com>
2018-05-06 20:12:35 +0200

    change write file to get intermediate result

--- test.py
+++ test.py
@@ -5,7 +5,9 @@ from path import Path
 with open("vcpkg_status.md", 'w') as outfile:
     outfile.write("| Library | x64-linux |\n")
     outfile.write("| :-- | -- |\n")
-    for lib in sorted(Path("vcpkg/ports").dirs("*")):
+
+for lib in sorted(Path("vcpkg/ports").dirs("*")):
+    with open("vcpkg_status.md", 'a') as outfile:
         ret = subprocess.run(["docker", "run", "base_vcpk", "./vcpkg", "install", lib.basename()])
         print(ret.args)
         outfile.write("| %s | %s |\n" % (lib.basename(), ["Failed", "Success"][ret.returncode == 0]))
