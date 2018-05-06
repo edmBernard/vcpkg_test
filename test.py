f16f037e291305eccdda9de008d1d996e261742e
Erwan BERNARD <bernard.erwan@gmail.com>
2018-05-06 20:46:46 +0200

    add rm in docker commandline

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 import subprocess
 from path import Path
 
-
 with open("vcpkg_status.md", 'w') as outfile:
     outfile.write("| Library | x64-linux |\n")
     outfile.write("| :-- | -- |\n")
 
 for lib in sorted(Path("vcpkg/ports").dirs("*")):
     with open("vcpkg_status.md", 'a') as outfile:
-        ret = subprocess.run(["docker", "run", "base_vcpk", "./vcpkg", "install", lib.basename()])
+        ret = subprocess.run(["docker", "run", "--name", lib.basename(), "--rm", "base_vcpk", "./vcpkg", "install", lib.basename()])
         print(ret.args)
         outfile.write("| %s | %s |\n" % (lib.basename(), ["Failed", "Success"][ret.returncode == 0]))
